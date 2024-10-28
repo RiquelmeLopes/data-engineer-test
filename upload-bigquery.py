b9115f090d110dc8b22122dfe4c78d0be6cfb86f
RiquelmeLopes <riqueque2010@hotmail.com>
2024-10-28 02:38:09 -0300

    Adição de comentários

--- upload-bigquery.py
+++ upload-bigquery.py
@@ -19,6 +19,7 @@ def upload_to_bigquery(json_file_path, dataset_id, table_id):
 
     table_ref = f"{client.project}.{dataset_id}.{table_id}"
 
+    # Testar para caso tenha erros, entender quais foram
     errors = client.insert_rows_json(table_ref, rows_to_insert)
     if errors == []:
         print("Os dados foram carregados com sucesso no BigQuery.")
