f6f82beb87b48e5a0de7c8bb9a671b4742095508
RiquelmeLopes <73597510+RiquelmeLopes@users.noreply.github.com>
2024-10-28 04:50:00 -0300

    Ajuste de identação e comentários

--- upload-bigquery.py
+++ upload-bigquery.py
@@ -19,12 +19,12 @@ def upload_to_bigquery(json_file_path, dataset_id, table_id):
 
     table_ref = f"{client.project}.{dataset_id}.{table_id}"
 
-    # Testar para caso tenha erros, entender quais foram
+    ## Testar para caso tenha erros, entender quais foram
     errors = client.insert_rows_json(table_ref, rows_to_insert)
     if errors == []:
         print("Os dados foram carregados com sucesso no BigQuery.")
     else:
         print("Erro ao carregar os dados:", errors)
 
-# Executar 
+## Executar 
 upload_to_bigquery('articles.json', 'data_eng_test', 'articles_table')
